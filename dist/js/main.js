18d8e4bcfd2373a1edf368cb0ada5811f5a01281
bugalu <47581329+bugalu@users.noreply.github.com>
2020-12-02 16:58:04 +0900

    Delete main.js

--- dist/js/main.js
+++
@@ -1,9 +0,0 @@
-const returnTop = document.querySelector(".js-returnTop");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= window.innerHeight / 2) {
-    returnTop.classList.add("visible");
-  } else if (window.scrollY < window.innerHeight / 2) {
-    returnTop.classList.remove("visible");
-  }
-});
